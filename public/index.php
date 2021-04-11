a46a17c4959bb84a98698c2a7f8cad156c1aa760
Your Name <you@example.com>
2021-04-12 00:04:52 +0300

    Auto commit

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
-error_reporting(E_ALL ^ E_WARNING);
-//ini_set('display_errors', 0);
-//ini_set('display_startup_errors', 0);
+//error_reporting(E_ALL ^ E_WARNING);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 require __DIR__ . '/../vendor/autoload.php';
 session_start();
 if (!isset($_SESSION['pex']))
