4f1baa0fad29b126c8a7dfc51d3717e3677df0d5
Your Name <you@example.com>
2021-04-12 01:04:29 +0300

    Auto commit

--- src/Views/layouts/app.blade.php
+++ src/Views/layouts/app.blade.php
@@ -6,7 +6,7 @@
             integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
             crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
-    <link href="./css.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="{{\App\Helper::url('css.css')}}" rel="stylesheet" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
